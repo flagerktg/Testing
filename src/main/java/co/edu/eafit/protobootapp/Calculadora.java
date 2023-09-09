10cf580842f24f9f76aaefd12d830955af27b256
Mauricio <mapoguti@gmail.com>
2023-09-09 09:32:50 -0500

    Jenkin test

---
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -0,0 +1,13 @@
+package co.edu.eafit.protobootapp;
+
+//This is a simple calculator class.
+public class Calculadora {
+
+    public Double cuadrado(Double numero) {
+        return numero * numero;
+    }
+    
+    public Double cubo(Double numero) {
+        return numero * numero * numero;
+    }
+}
